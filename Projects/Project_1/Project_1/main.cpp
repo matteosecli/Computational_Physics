93fc61ad5b141b6dba72c19a5a82c3fd0c7af36c
Matteo Seclì <secli.matteo@gmail.com>
2014-09-04 12:54:18 +0200

    Project 1 - Added LU decomposition and standard Gaussian elimination

--- Projects/Project_1/Project_1/main.cpp
+++ Projects/Project_1/Project_1/main.cpp
@@ -9,6 +9,71 @@ using namespace std;
 using namespace arma;
 namespace use {int onealg = 0; int out = 1;}
 
+// 'fill_matrix' fills the matrix A in a tridiagonal form.
+void fill_matrix(mat& A, int N) {
+    //Fill the matrix
+    A(0,0) = 2;
+    A(0,1) = -1;
+    for(int i = 1; i < N-1; i++){
+        A(i,i-1) = -1;
+        A(i,i) = 2;
+        A(i,i+1) = -1;
+    }
+    A(N-1,N-2) = -1;
+    A(N-1,N-1) = 2;
+}
+
+// 'solve_gaus' solves the system with a standard gaussian decomposition
+void solve_gaus(vec& u, vec& f, int N){
+    //Start timing
+    clock_t t;
+    t = clock();
+
+    //Define our matrix and initialize it
+    mat A(N,N);
+    A.zeros();
+    fill_matrix(A,N);
+
+    //Just the decomposition
+    u = solve(A,f);
+    A.reset();
+
+     //Stop timing
+    t = clock() - t;
+    cout <<"Elapsed time (solve_gaus):\t\t" <<((float)t)/CLOCKS_PER_SEC <<"s." <<endl;
+}
+
+// 'solve_lu' solves the system with a standard LU decomposition
+void solve_lu(vec& u, vec& f, int N){
+    //Start timing
+    clock_t t;
+    t = clock();
+
+    //Define our matrix and initialize it
+    mat A(N,N);
+    A.zeros();
+    fill_matrix(A,N);
+
+    //Define workspace matrices
+    mat L(N,N), U(N,N), P(N,N);
+
+    //Do the decomposition
+    lu(L, U, P, A);
+    A.reset();
+
+    //Just solve the system
+    vec b;
+    b = solve(L,P*f);
+    L.reset();
+    P.reset();
+    u = solve(U,b);
+    U.reset();
+
+    //Stop timing
+    t = clock() - t;
+    cout <<"Elapsed time (solve_lu):\t\t" <<((float)t)/CLOCKS_PER_SEC <<"s." <<endl;
+}
+
 // 'solvetrid' is a function that solves a linear sistem in 'u' relative to a
 // tridiagonal matrix with diagonal elements equal to 'b', subdiagonal elements
 // equal to 'a' and superdiagonal elements equal to 'c'. Returns the result
@@ -88,9 +153,10 @@ vec relative_error(vec& u, vec& x, int& N) {
 }
 
 // 'main' takes as first argumt the number of points that the program
-// will use during the calculation. Use 'nofile' as second argument
-// if you don't want to write output to file (useful for benchmarking
-// purposes).
+// will use during the calculation. Use 'onealg 1' as second argument
+// if you want to use only one algorithm and write to the output file.
+// Use 'anealg 0' if you want to use only one algorithm and don't write
+// to the output file.
 int main(int argc, char *argv[])
 {
     int N = atoi(argv[1]);
@@ -120,10 +186,14 @@ int main(int argc, char *argv[])
     // Compare algorithms only if we want to do benchmarks.
     // This is to save memory if we want just to have grid numbers.
     if(use::onealg == 0) {
+        // Solve using 'solve_lu' and 'solve_gaus'
+        if (N <= 10000) {
+            solve_lu(u,f,N);
+            solve_gaus(u,f,N);
+        }
+
         // Solve using 'tridig'
-        u_temp = u;
-        solvetrid(N, a, b, c, u_temp, f);
-        u_temp.reset();
+        solvetrid(N, a, b, c, u, f);
     }
 
     // Solve using 'solve_special'
--- Projects/Project_1/README.md
+++ Projects/Project_1/README.md
@@ -2,7 +2,10 @@ Project 1
 =========
 *Solving a simple PDE*
 
+Building:
+`g++ main.cpp -o Project_1 -O3 -larmadillo -llapacke -llapack -lgfortran`
+
 Usage:
-- `$ ./Project_1 N` solves the PDE using N points with two different algorithms and doesn't write output to file (useful for benchmarking purposes).
+- `$ ./Project_1 N` solves the PDE using N points with different algorithms and doesn't write output to file (useful for benchmarking purposes).
 - `$ ./Project_1 N onealg 1` uses the fastest algorithm and writes output.
 - `$ ./Project_1 N onealg 0` uses the fastest algorithm and doesn't write output (useful for testing speed on large N).
